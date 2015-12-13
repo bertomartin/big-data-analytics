d3341cc922820d400e08e72421f7016a8a4e2ed3
Roberto <rmartin.martin@gmail.com>
2015-12-13 05:42:31 -0500

    added features: price over 10ema, price momentum, volume momentum

--- features.py
+++ features.py
@@ -33,13 +33,46 @@ def price_above_20(df, feature_dict):
     ma20['price_above'] = np.where(ma20['close'] - ma20['MA_20'] > 0, 1, 0)
     grouped = ma20.groupby(['fiscal_year', 'fiscal_quarter'])[['MA_20']]
     for k, val in grouped:
-        cnt = val['MA_20'].count()
-        sm = val['MA_20'].sum()
+        cnt = val['price_above'].count()
+        sm = val['price_above'].sum()
         feature_dict[k].append(1 if float(sm)/cnt > 0.5 else 0)
-    # pdb.set_trace()
+        # pdb.set_trace()
 
-def num_vol_up_days(df, feature_dict):
-    pass
+# percentage of times price was above 10d ema above 50?
+def price_above_10_ema(df, feature_dict):
+    ma10_ema = EMA(df,10)
+    ma10_ema = ma10_ema.dropna()
+    ma10_ema['price_above'] = np.where(ma10_ema['close'] - ma10_ema['EMA_10'] > 0, 1, 0)
+    grouped = ma10_ema.groupby(['fiscal_year', 'fiscal_quarter'])[['EMA_10']]
+    for k, val in grouped:
+        cnt = val['price_above'].count()
+        sm = val['price_above'].sum()
+        feature_dict[k].append(1 if float(sm)/cnt > 0.5 else 0)
+
+# percentage of times price went up was above 50?
+def price_mom1(df, feature_dict):
+    '''
+    returns the number of days the price increase
+    '''
+    mom1 = MOM(df,1)
+    mom1 = mom1.dropna()
+    mom1['mom'] = np.where(mom1['momentum_1'] > 0, 1, 0)
+    grouped = mom1.groupby(['fiscal_year', 'fiscal_quarter'])[['mom']]
+    for k, val in grouped:
+        cnt = val['mom'].count()
+        sm = val['mom'].sum()
+        feature_dict[k].append(1 if float(sm)/cnt > 0.5 else 0)
+
+# percentage of times volume went up was over 50?
+def vol_mom1(df, feature_dict):
+    mom1 = MOM_vol(df,1)
+    mom1 = mom1.dropna()
+    mom1['v_mom'] = np.where(mom1['v_momentum_1'] > 0, 1, 0)
+    grouped = mom1.groupby(['fiscal_year', 'fiscal_quarter'])[['v_mom']]
+    for k, val in grouped:
+        cnt = val['v_mom'].count()
+        sm = val['v_mom'].sum()
+        feature_dict[k].append(1 if float(sm)/cnt > 0.5 else 0)
 
 def num_obv_up_days(df, feature_dict):
     pass
@@ -56,15 +89,22 @@ def all_features(feature_file, combined_file):
     #features
     up_days(df, feature_dict, target_dict) #feature_dict should be updated
     price_above_20(df, feature_dict)
+    price_above_10_ema(df, feature_dict)
+    price_mom1(df, feature_dict)
+    vol_mom1(df, feature_dict)
 
-    print_str = "yr,qtr,up_day,p_over_20,target\n"
+
+    print_str = "yr,qtr,up_day,p_over_20,p_over_10_ema,p_mom_1,v_mom_1,target\n"
     for item in feature_dict.items():
         key = item[0]
         yr, qtr = key
         target = target_dict[key] #beat for this yr,qtr
         up_day = feature_dict[key][0] #up day is the first feature
         p_over_20 = feature_dict[key][1]
-        print_str = print_str + "{0},{1},{2},{3},{4}\n".format(yr,qtr,up_day,p_over_20,target)
+        p_above_10_ema = feature_dict[key][2]
+        p_mom_1 = feature_dict[key][3]
+        v_mom_1 = feature_dict[key][4]
+        print_str = print_str + "{0},{1},{2},{3},{4},{5},{6},{7}\n".format(yr,qtr,up_day,p_over_20,p_above_10_ema,p_mom_1,v_mom_1,target)
     fout = open(feature_file, 'wt')
     fout.write(print_str)
     fout.close()
