41ae48c62d37cdc75c2b94f2103b399768a04a04
Roberto <rmartin.martin@gmail.com>
2015-12-13 22:26:11 -0500

    training a decision tree and a logistic model

---
+++ model/models_app.py
@@ -0,0 +1,80 @@
+"""models_app.py"""
+
+from pyspark import SparkContext
+from pyspark.mllib.classification import LogisticRegressionWithLBFGS
+from pyspark.mllib.tree import DecisionTree
+from pyspark.mllib.evaluation import BinaryClassificationMetrics
+from pyspark.mllib.regression import LabeledPoint
+from numpy import array
+from time import time
+import pdb
+
+sc = SparkContext("local", "Simple App")
+# logData = sc.textFile(logFile).cache()
+
+training_data_file = "/Users/roberto/Desktop/final-project/model/train_noheader.csv"
+training_data_raw = sc.textFile(training_data_file)
+
+print "Test data size is {}".format(training_data_raw.count())
+
+def parse_lines(line):
+    line_split = line.split(",")
+    clean_line_split = line_split[1:7]
+    target = line_split[7]
+    return LabeledPoint(target, array([float(x) for x in clean_line_split]))
+
+training_data = training_data_raw.map(parse_lines)
+
+
+# Build the model
+###############Logistic Regression Model ########################
+# run training algorithm to build the model
+t0 = time()
+logit_model = LogisticRegressionWithLBFGS.train(training_data)
+tt = time() - t0
+print "Logistic Classifier trained in {} seconds".format(round(tt,3))
+
+# compute the labels and predictions
+labels_and_preds = training_data.map(lambda p: (float(logit_model.predict(p.features)), p.label  ))
+
+# Instantiate metrics
+metrics = BinaryClassificationMetrics(labels_and_preds)
+
+# Area under precision-recall curve
+print "Area under PR (Logistic) = {}".format(metrics.areaUnderPR * 100.0)
+
+# Area under ROC curve
+print "Area under ROC (Logistic) = {}".format(metrics.areaUnderROC * 100.0)
+
+# compute accuracy
+training_accuracy = labels_and_preds.filter(lambda (v,p): v == p).count() / float(training_data.count())
+
+print "Training accuracy {}".format(round(training_accuracy, 4))
+###############/Logistic Regression #########################
+
+#################### Decision Tree ############################
+
+t0 = time()
+dt_model = DecisionTree.trainClassifier(training_data, numClasses=2, categoricalFeaturesInfo={},
+                                     impurity='gini', maxDepth=5, maxBins=32)
+
+tt = time() - t0
+print "Decision Tree Classifier trained in {} seconds".format(round(tt,3))
+
+predictions = dt_model.predict(training_data.map(lambda p: p.features))
+labels_and_preds = training_data.map(lambda p: p.label).zip(predictions)
+
+# Instantiate metrics
+dt_metrics = BinaryClassificationMetrics(labels_and_preds)
+# Area under precision-recall curve
+print "Area under PR (Decision Tree) = {}".format(dt_metrics.areaUnderPR * 100.0)
+
+# Area under ROC curve
+print "Area under ROC (Decision Tree) = {}".format(dt_metrics.areaUnderROC * 100.0)
+
+
+t0 = time()
+training_accuracy = labels_and_preds.filter(lambda (v, p): v == p).count() / float(training_data.count())
+
+print "Prediction made in {} seconds. Test accuracy is {}".format(round(tt,3), round(training_accuracy,4))
+#################### /Decision Tree ##########################
