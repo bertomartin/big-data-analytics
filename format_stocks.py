3178b09850440e2f30eb9b95f12afb15a4b51ef0
Roberto <rmartin.martin@gmail.com>
2015-12-14 23:12:28 -0500

    updating stock formatting code to generalize to all stocks

--- format_stocks.py
+++ format_stocks.py
@@ -2,20 +2,22 @@ import pandas as pd
 import numpy as np
 import csv
 from dateutil.parser import parse
+from glob import glob
 import pdb
 
-def format_ohlc(ohlc_file):
+def format_ohlc():
     '''(string) -> None
 
     removes the time column and indexes the dataframe by date. overwrites stock file
     with this new information
     '''
-    df = pd.read_csv(ohlc_file)
-    df.drop('time', axis=1, inplace=True)
-    df.index=df['date'].apply(parse)
-    df.drop('date', axis=1, inplace=True)
-    df.to_csv(ohlc_file)
-
+    stock_files = glob('./stocks/*')
+    for f in stock_files:
+        df = pd.read_csv(f)
+        df.drop('time', axis=1, inplace=True)
+        df.index=df['date'].apply(parse)
+        df.drop('date', axis=1, inplace=True)
+        df.to_csv(f)
 
 if __name__ == "__main__":
-    df = format_ohlc("./test-stocks/ADBE.csv")
+    format_ohlc()
