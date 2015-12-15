8de755e3c111e65974567ee13cd993f132c70278
Roberto <rmartin.martin@gmail.com>
2015-12-14 23:48:34 -0500

    updating format_estimize to loop through files

--- format_estimize.py
+++ format_estimize.py
@@ -2,25 +2,23 @@ import pandas as pd
 import numpy as np
 import csv
 from dateutil.parser import parse
+from glob import glob
 import pdb
-def remove_lines(file_name):
+
+def remove_lines():
     '''(string) -> None
 
     Remove the years 2017, 2016, 2015 and 1999 from estimze files
     '''
-    df = pd.read_csv(file_name)
-    mask = ((df['fiscal_year'] == 2017) | (df['fiscal_year'] == 2016) | (df['fiscal_year'] == 2015) | (df['fiscal_year'] == 1999))
-    rev_mask = np.invert(mask)
-    df2 = df[rev_mask] #new data frame with only what we want
-    df2.index=df2['date'].apply(parse)
-    return df2
-
-def write_to_csv(df, file_name):
-    '''(DataFrame, file_name) -> None
-
-    writes the dataframe to csv file
-    '''
-    df.to_csv(file_name)
+    estimize_files = glob('./estimize/*')
+    for f in estimize_files:
+        df = pd.read_csv(f, na_values=['None']).fillna(0)
+        mask = ((df['fiscal_year'] == 2018) | (df['fiscal_year'] == 2017) | (df['fiscal_year'] == 2016) | (df['fiscal_year'] == 2015) | (df['fiscal_year'] == 1999))
+        rev_mask = np.invert(mask)
+        df2 = df[rev_mask] #new data frame with only what we want
+        df2.index=df2['date'].apply(parse)
+        df3 = calculate_beat(df2)
+        df3.to_csv(f)
 
 def calculate_beat(df):
     '''(DataFrame) -> DataFrame
@@ -30,9 +28,6 @@ def calculate_beat(df):
     '''
     df['beat'] = np.where(df['eps'].map(float) - df['wallstreet_estimates'].map(float) > 0, 1, 0)
     return df
-    # pdb.set_trace()
 
 if __name__ == "__main__":
-    df = remove_lines("./test-stocks/ADBE_estimize.csv")
-    df1 = calculate_beat(df)
-    write_to_csv(df1, "./test-stocks/ADBE_estimize.csv")
+    remove_lines()
